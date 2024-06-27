a126804f5d2b6a448a76eb22dd3736283f0cdf28
Saurav Pandey <133730941+Sauravpandey261@users.noreply.github.com>
2024-06-27 14:33:47 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,94 @@
+let items = document.querySelectorAll('.slider .list .item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+let thumbnails = document.querySelectorAll('.thumbnail .item');
+
+// config param
+let countItem = items.length;
+let itemActive = 0;
+// event next click
+next.onclick = function(){
+    itemActive = itemActive + 1;
+    if(itemActive >= countItem){
+        itemActive = 0;
+    }
+    showSlider();
+}
+//event prev click
+prev.onclick = function(){
+    itemActive = itemActive - 1;
+    if(itemActive < 0){
+        itemActive = countItem - 1;
+    }
+    showSlider();
+}
+// auto run slider
+let refreshInterval = setInterval(() => {
+    next.click();
+}, 5000)
+function showSlider(){
+    // remove item active old
+    let itemActiveOld = document.querySelector('.slider .list .item.active');
+    let thumbnailActiveOld = document.querySelector('.thumbnail .item.active');
+    itemActiveOld.classList.remove('active');
+    thumbnailActiveOld.classList.remove('active');
+
+    // active new item
+    items[itemActive].classList.add('active');
+    thumbnails[itemActive].classList.add('active');
+
+    // clear auto time run slider
+    clearInterval(refreshInterval);
+    refreshInterval = setInterval(() => {
+        next.click();
+    }, 5000)
+}
+
+// click thumbnail
+thumbnails.forEach((thumbnail, index) => {
+    thumbnail.addEventListener('click', () => {
+        itemActive = index;
+        showSlider();
+    })
+})
+
+
+gsap.to(".mainnav ",{
+    backgroundColor:"white",
+    color:"black",
+    duration:0.5,
+    height:"110px",
+    scrollTrigger:{
+        trigger:".nav",
+        scroller:"body",
+        start:"top -10%",
+        end:"top -9%",
+        scrub:1
+    },
+})
+gsap.to(".middle a",{
+    color:"black",
+    duration:0.5,
+    scrollTrigger:{
+        trigger:".nav",
+        scroller:"body",
+        start:"top -10%",
+        end:"top -9%",
+        scrub:1
+    }
+})
+gsap.to(".maindropdownmenu",{
+    backgroundColor: "rgba(79, 174, 128, 0.513)",
+    color:"white",
+    duration:0.5,
+    scrollTrigger:{
+        trigger:".maindropdownmenu",
+        scroller:"body",
+        start:"top -5%",
+        end:"top -9%",
+        scrub:1
+    }
+})
+
+
+
